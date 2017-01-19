5995e81a4361cafbb0d6f15be314299c173cb8d0
woong <anguish71@gmail.com>
2017-01-20 00:43:26 +0900

    Implement java traingle with JS/Decorate star with JS/Make elements of desktop objects(need revision for assignment6

--- Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
+++ Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
@@ -1,14 +1,66 @@
-var Desktop = function() {
-	/* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+var Desktop = function(imgNum,folderNum) {
+  this.imgNum = imgNum;
+  this.folderNum = folderNum;
+  this.imgList = new Array();
+  this.folderList = new Array();
+
+  for(var i=0;i<imgNum;i++){
+    var name = Math.random().toString(36).substr(2,5) + '.jpg';
+    this.imgList[i] = new Img(name,'img');
+  }
+
+  for(var j=0;j<folderNum;j++){
+    var name = Math.random().toString(36).substr(2,5) + '.folder';
+    this.folderList[j] = new Folder(name,'folder');
+  }
+
 };
 
-var Icon = function() {
-	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+var Icon = function(name,type) {
+  this.name = name;
+  this.type = type;
 };
 
-var Folder = function() {
-	/* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+
+var Folder = function(name,type) {
+  Icon.call(this, name, type);
+
+
+  var folder = document.createElement("div");
+  folder.className = "icon folder";
+  var txt = document.createTextNode(this.name);
+  folder.appendChild(txt);
+
+  var main = document.querySelector(".desktop");
+  main.appendChild(folder);
+
 };
+
+
+//inherit Icon
+Folder.prototype = new Icon();
+
+//correct the constructor pointer because it points to Icon
+Folder.prototype.constructor = Folder;
+
+var Img = function(name,type) {
+  Icon.call(this, name, type);
+
+  var img = document.createElement("div");
+  img.className = "icon";
+  var txt = document.createTextNode(this.name);
+  img.appendChild(txt);
+
+  var main = document.querySelector(".desktop");
+  main.appendChild(img);
+
+}
+
+//inherit Icon
+Img.prototype = new Icon();
+
+//correct the constructor pointer because it points to Icon
+Img.prototype.constructor = Img;
 
 var Window = function() {
 	/* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
