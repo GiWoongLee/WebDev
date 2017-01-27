ecc77d71a4c8d5531933b5c1fa3e400030ec18b3
woong <anguish71@gmail.com>
2017-01-27 18:27:03 +0900

    Add drag&drop function to icons/refactoring code with prototypal inheritance

--- Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
+++ Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
@@ -1,3 +1,12 @@
+/*
+  Reference for JavaScript Prototype vs Class Inheritance
+  ES6 class Inheritnace - https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Classes
+  Prototypal vs Class Inheritnace -https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Details_of_the_Object_Model
+  https://medium.com/javascript-scene/master-the-javascript-interview-what-s-the-difference-between-class-prototypal-inheritance-e4cd0a7562e9#.cyvadvgtr
+
+*/
+
+
 var Desktop = function(imgNum,folderNum) {
   this.imgNum = imgNum;
   this.folderNum = folderNum;
@@ -6,54 +15,84 @@ var Desktop = function(imgNum,folderNum) {
 
   for(var i=0;i<imgNum;i++){
     var name = Math.random().toString(36).substr(2,5) + '.jpg';
-    this.imgList[i] = new Img(name,'img');
+    this.imgList[i] = new Img(name,'img',i);
+    this.imgList[i].makeComponent();
   }
 
   for(var j=0;j<folderNum;j++){
     var name = Math.random().toString(36).substr(2,5) + '.folder';
-    this.folderList[j] = new Folder(name,'folder');
+    this.folderList[j] = new Folder(name,'folder',imgNum+j);
+    this.folderList[j].makeComponent();
   }
 
 };
 
-var Icon = function(name,type) {
+var Icon = function(name,type,index) {
   this.name = name;
   this.type = type;
 };
 
+Icon.prototype.makeComponent = function(){
+  //constructor for Component
+  var self = this;
+  var icon = document.createElement("div");
+  if(this.type=='folder'){
+    icon.className = "icon folder"
+  }
+  else{
+    icon.className = "icon";
+  }
+  var txt = document.createTextNode(this.name);
+  icon.appendChild(txt);
+  var main = document.querySelector(".desktop");
+  main.appendChild(icon);
+  icon.addEventListener("mousedown",this.movPos);
+}
+
+//Giving Property - Method - to Folder
+Icon.prototype.movPos = function(e){
+  e = e || event;
+  var self = this;
+
+  /* Remove offsetTop because of positioning BLOCK style*/
+  var dragData = {
+        x: e.pageX - self.offsetLeft,
+        y: e.pageY
+  };
+
+  function drag(e){
+    self.style.left = e.pageX - dragData.x + "px";
+    self.style.top = e.pageY - dragData.y + "px";
+  }
+
+  document.addEventListener("mousemove",drag);
+
+  document.addEventListener("mouseup",function(e){
+    document.removeEventListener('mousemove',drag);
+  });
+}
+
 
 var Folder = function(name,type) {
+  //inheritance
   Icon.call(this, name, type);
 
-
-  var folder = document.createElement("div");
-  folder.className = "icon folder";
-  var txt = document.createTextNode(this.name);
-  folder.appendChild(txt);
-
-  var main = document.querySelector(".desktop");
-  main.appendChild(folder);
-
+  //field
+  this.fileNum = 0;
+  this.fileList = new Array();
+  this.window = new Window();
 };
 
-
 //inherit Icon
-Folder.prototype = new Icon();
+//Inheriting properties of Icon Prototype to Folder Prototype
+Folder.prototype = Object.create(Icon.prototype);
 
 //correct the constructor pointer because it points to Icon
 Folder.prototype.constructor = Folder;
 
+
 var Img = function(name,type) {
   Icon.call(this, name, type);
-
-  var img = document.createElement("div");
-  img.className = "icon";
-  var txt = document.createTextNode(this.name);
-  img.appendChild(txt);
-
-  var main = document.querySelector(".desktop");
-  main.appendChild(img);
-
 }
 
 //inherit Icon
