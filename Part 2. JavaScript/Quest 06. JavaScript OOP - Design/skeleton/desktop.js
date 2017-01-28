fd744ec42810df11f61164f222951cb70fe60195
woong <anguish71@gmail.com>
2017-01-28 22:42:54 +0900

    Modify drag&drop function due to error of relative positioning

--- Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
+++ Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
@@ -8,20 +8,20 @@
 
 
 var Desktop = function(imgNum,folderNum) {
-  this.imgNum = imgNum;
-  this.folderNum = folderNum;
+  this.imgNum = Number(imgNum);
+  this.folderNum = Number(folderNum);
   this.imgList = new Array();
   this.folderList = new Array();
 
-  for(var i=0;i<imgNum;i++){
+  for(var i=0;i<this.imgNum;i++){
     var name = Math.random().toString(36).substr(2,5) + '.jpg';
     this.imgList[i] = new Img(name,'img',i);
     this.imgList[i].makeComponent();
   }
 
-  for(var j=0;j<folderNum;j++){
+  for(var j=0;j<this.folderNum;j++){
     var name = Math.random().toString(36).substr(2,5) + '.folder';
-    this.folderList[j] = new Folder(name,'folder',imgNum+j);
+    this.folderList[j] = new Folder(name,'folder',this.imgNum+j);
     this.folderList[j].makeComponent();
   }
 
@@ -30,18 +30,22 @@ var Desktop = function(imgNum,folderNum) {
 var Icon = function(name,type,index) {
   this.name = name;
   this.type = type;
+  this.index = index;
 };
 
 Icon.prototype.makeComponent = function(){
   //constructor for Component
   var self = this;
   var icon = document.createElement("div");
+
   if(this.type=='folder'){
-    icon.className = "icon folder"
+    icon.className = "icon folder";
   }
   else{
     icon.className = "icon";
   }
+  icon.id = self.index;
+
   var txt = document.createTextNode(this.name);
   icon.appendChild(txt);
   var main = document.querySelector(".desktop");
@@ -54,28 +58,33 @@ Icon.prototype.movPos = function(e){
   e = e || event;
   var self = this;
 
-  /* Remove offsetTop because of positioning BLOCK style*/
   var dragData = {
-        x: e.pageX - self.offsetLeft,
-        y: e.pageY
+    x: e.pageX - self.offsetLeft,
+    y: e.pageY - self.offsetTop
   };
+
+  //Due to the Relative Positioning, move element considering relative position
+  var iconHeight = document.getElementById(self.id).offsetHeight;
+  var eleRelaPos = self.id * iconHeight;
 
   function drag(e){
     self.style.left = e.pageX - dragData.x + "px";
-    self.style.top = e.pageY - dragData.y + "px";
+    self.style.top = e.pageY - dragData.y - eleRelaPos + "px";
   }
+
 
   document.addEventListener("mousemove",drag);
 
   document.addEventListener("mouseup",function(e){
     document.removeEventListener('mousemove',drag);
   });
+
 }
 
 
-var Folder = function(name,type) {
+var Folder = function(name,type,index) {
   //inheritance
-  Icon.call(this, name, type);
+  Icon.call(this, name, type,index);
 
   //field
   this.fileNum = 0;
@@ -91,12 +100,12 @@ Folder.prototype = Object.create(Icon.prototype);
 Folder.prototype.constructor = Folder;
 
 
-var Img = function(name,type) {
-  Icon.call(this, name, type);
+var Img = function(name,type,index) {
+  Icon.call(this, name, type,index);
 }
 
 //inherit Icon
-Img.prototype = new Icon();
+Img.prototype = Object.create(Icon.prototype);
 
 //correct the constructor pointer because it points to Icon
 Img.prototype.constructor = Img;
