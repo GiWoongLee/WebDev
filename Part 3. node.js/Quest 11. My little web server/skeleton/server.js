4113964a60b7dc426d33f9989f5a2e31e841d687
woong <anguish71@gmail.com>
2017-02-04 00:52:34 +0900

    Implement basic server with NodeJS(Quest10/11)

--- Part 3. node.js/Quest 11. My little web server/skeleton/server.js
+++ Part 3. node.js/Quest 11. My little web server/skeleton/server.js
@@ -1,5 +1,73 @@
 var http = require('http');
+var url = require('url');
 
-http.createServer(function(req, res) {
-	// TODO: 이 곳을 채워넣으세요..!
+//HTTP Server
+var server = http.createServer(function(req, res){
+  res.setHeader("Content-Type","text/html");
+  res.writeHead(200);
+
+  if(req.method == 'GET'){
+    var query = url.parse(req.url,true).query;
+    res.write("Hello " + query.bar);
+    res.end();
+  }
+
+  else{
+    var jsonData = "";
+    req.on('data',function(chunk){
+      jsonData += chunk;
+    });
+
+    req.on('end',function(){
+      var reqObj = JSON.parse(jsonData);
+      var resObj = {
+        bar : "woong"
+      };
+      res.end(JSON.stringify(resObj));
+    });
+  }
 }).listen(8080);
+
+
+
+
+//HTTP Client
+
+var option1 = {
+  hostname: 'localhost',
+  port : '8080',
+  path : '/foo?bar=kyuin'
+};
+
+var getReq = http.request(option1,function(response){
+  var responseData = "";
+  response.on('data',function(chunk){
+    responseData += chunk;
+  });
+  response.on('end',function(){
+    console.log(responseData);
+  });
+});
+
+getReq.end();
+
+var option2 = {
+  hostname: 'localhost',
+  port: '8080',
+  path: '/foo',
+  method: 'POST'
+};
+
+var postReq = http.request(option2,function(response){
+  var responseData = "";
+  response.on('data',function(chunk){
+    responseData += chunk;
+  });
+  response.on('end',function(){
+    var dataObj = JSON.parse(responseData);
+    console.log("Hello " + dataObj.bar);
+  });
+});
+
+postReq.write('{"bar" : "woong" }');
+postReq.end();
