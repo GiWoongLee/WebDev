8b8ccc309efbd616195213e0fd8919bb02e2ff3b
woong <anguish71@gmail.com>
2017-02-05 21:30:10 +0900

    Add three functions new/load/save of notepad(Quest12)

--- Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
@@ -1,20 +1,21 @@
 var Notepad = function() {
 	 //createButtons
-   var funcList = document.createElement("ul");
+   var navList = document.createElement("ul");
    var func1 = new newFunc();
    var func2 = new loadFunc();
    var func3 = new saveFunc();
-   funcList.appendChild(func1);
-   funcList.appendChild(func2);
-   funcList.appendChild(func3);
+   navList.appendChild(func1);
+   navList.appendChild(func2);
+   navList.appendChild(func3);
+   navList.className = "navList";
 
    //createTextArea
    var txtArea = new textArea();
 
    var main = document.querySelector(".notepad");
-   main.appendChild(funcList);
+   main.appendChild(navList);
    main.appendChild(txtArea);
-
+   document.title = "New Note";
 };
 
 var textArea = function(){
@@ -37,15 +38,14 @@ textArea.readText = function(){
   return txt.value;
 };
 
-textArea.refreshText = function(){
-  var content = textArea.readText();
-  textArea.removeText();
-  return content;
+textArea.refreshText = function(content){
+  var txt = document.getElementById("note");
+  txt.value = content;
 };
 
 var func = function(){
   var func = document.createElement("li");
-  func.className = "funcEle";
+  func.className = "navEle";
   return func;
 };
 
@@ -57,6 +57,7 @@ var newFunc = function(){
   ele.appendChild(txt);
   ele.addEventListener("click",function(){
     textArea.removeText();
+    document.title = "New Note";
   })
   return ele;
 };
@@ -70,6 +71,9 @@ var loadFunc = function(){
   ele.name = "load";
   var txt = document.createTextNode("Load");
   ele.appendChild(txt);
+  ele.addEventListener("click",function(){
+    new loadWindo();
+  })
   return ele;
 };
 
@@ -82,6 +86,9 @@ var saveFunc = function(){
   ele.name = "save";
   var txt = document.createTextNode("Save");
   ele.appendChild(txt);
+  ele.addEventListener("click",function(){
+    new saveWindo();
+  });
   return ele;
 };
 
@@ -90,9 +97,121 @@ saveFunc.prototype = Object.create(func.prototype);
 saveFunc.prototype.constructor = saveFunc;
 
 var loadWindo = function(){
+  var windo = document.createElement("div");
+  windo.id = "loadwindo";
+
+  var div = document.createElement("div");
+  div.className = "fileTitle";
+  var txt= document.createTextNode("FileName");
+  div.appendChild(txt);
+  windo.appendChild(div);
+
+
+  var fileList = document.createElement("ul");
+  fileList.id = "loadList";
+  windo.appendChild(fileList);
+
+  var req = new XMLHttpRequest();
+  req.open("GET","/show",true);
+  req.onreadystatechange = function(e){
+    if(req.readyState==4 && req.status == 200){
+      var json = JSON.parse(req.responseText);
+
+      for(var i=0;i<json.length;i++){
+        (function(j){
+          var ele = document.createElement("li");
+
+          ele.addEventListener("dblclick",function(e){
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST","/load",true);
+            xhr.onreadystatechange = function(){
+              if(xhr.readyState==4 && xhr.status == 200){
+                var main = document.querySelector(".notepad");
+                main.removeChild(windo);
+                document.title = ele.textContent;
+                textArea.refreshText(xhr.responseText);
+              }
+            }
+            var fileName = ele.textContent;
+            xhr.setRequestHeader("content-type","application/json");
+            xhr.send(JSON.stringify({"name" : fileName}));
+
+          });
+
+          ele.className = "fileEle";
+          var txt = document.createTextNode(json[j]);
+          ele.appendChild(txt);
+          fileList.appendChild(ele);
+      })(i)
+      }
+    }
+  }
+  //후에 사용자가 생기면 사용자 이름도 보내기
+  req.send(null);
+
+  var message = document.createElement("div");
+  message.className = "message";
+  var txt = document.createTextNode("Double Click Note you want to load");
+  message.appendChild(txt);
+  windo.appendChild(message);
+
+  var main = document.querySelector(".notepad");
+  main.appendChild(windo);
+
 
 };
 
-var saveWindo = function(){
 
+var saveWindo = function(){
+  var windo = document.createElement("div");
+  windo.id = "saveWindo";
+
+  var txt = document.createTextNode("Name: ");
+  var inputName = document.createElement("input");
+  inputName.id = "fileName";
+  inputName.type = "text";
+  var br = document.createElement("br");
+
+  var submitBut = document.createElement("input");
+  submitBut.className = "fileBut";
+  submitBut.type = "button";
+  submitBut.value = "Save";
+
+  submitBut.addEventListener('click',function(event){
+    var content = textArea.readText();
+    var req = new XMLHttpRequest();
+    req.open("POST",'/save',true);
+    req.onreadystatechange = function(e){
+      if(req.readyState==4 && req.status == 200){
+        var main = document.querySelector(".notepad");
+        main.removeChild(windo);
+        document.title = inputName.value;
+      }
+    }
+    req.setRequestHeader('content-type','application/json');
+    req.send(JSON.stringify({
+      name : inputName.value,
+      content : content
+    }));
+  });
+
+  var cancelBut = document.createElement("input");
+  cancelBut.className = "fileBut";
+  cancelBut.type = "button";
+  cancelBut.value = "Cancel";
+
+  cancelBut.addEventListener('click',function(event){
+    var main = document.querySelector(".notepad");
+    main.removeChild(windo);
+  })
+
+  windo.appendChild(txt);
+  windo.appendChild(inputName);
+  windo.appendChild(br);
+  windo.appendChild(submitBut);
+  windo.appendChild(cancelBut);
+
+
+  var main = document.querySelector(".notepad");
+  main.appendChild(windo);
 };
--- Part 3. node.js/Quest 12. AJAX/skeleton/server.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/server.js
@@ -1,14 +1,44 @@
 var express = require('express'),
 	path = require('path'),
+	bodyParser = require('body-parser'),
+	fs = require('fs'),
 	app = express();
 
+
 app.use(express.static('client'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-/* TODO: 여기에 처리해야 할 요청의 주소별로 동작을 채워넣어 보세요..! */
+app.get('/show',function(req,res){
+	var newPath = __dirname + "/notes/";
+	fs.readdir(newPath,function(err,files){
+		if(err) return console.log(err);
+		else{
+			res.send(files);
+		}
+	});
+});
+
+app.post('/load',function(req,res){
+	var newPath = __dirname + "/notes/" + req.body.name;
+	fs.readFile(newPath,function(err,data){
+		if(err) return console.log(err);
+		else res.send(data);
+	})
+});
+
+app.post('/save',function(req,res){
+	var newPath = __dirname + "/notes/" + req.body.name;
+	fs.writeFile(newPath,req.body.content,function(err){
+		if(err) return console.log(err);
+	});
+	res.send(null);
+});
+
 
 var server = app.listen(8080, function () {
 	console.log('Server started!');
