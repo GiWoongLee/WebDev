632727561bacdb366f48ede7f65035c27c6fa79d
woong <anguish71@gmail.com>
2017-02-05 16:03:40 +0900

    Make basic DOMs of Notepad(Quest12)

--- Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
@@ -1,3 +1,80 @@
 var Notepad = function() {
-	/* TODO: 그 외에 또 어떤 클래스와 메소드가 정의되어야 할까요? */
+	 //createButtons
+   var funcList = document.createElement("ul");
+   var func1 = new newFunc();
+   var func2 = new loadFunc();
+   var func3 = new saveFunc();
+   funcList.appendChild(func1);
+   funcList.appendChild(func2);
+   funcList.appendChild(func3);
+
+   //createTextArea
+   var txtArea = new textArea();
+
+   var main = document.querySelector(".notepad");
+   main.appendChild(funcList);
+   main.appendChild(txtArea);
+
+};
+
+var textArea = function(){
+  var txtArea = document.createElement("div");
+  txtArea.className = "noteArea";
+  var txtNode = document.createElement("textarea");
+  txtArea.appendChild(txtNode);
+  txtNode.className = "note";
+
+  return txtArea;
+};
+
+
+var func = function(){
+  var func = document.createElement("li");
+  func.className = "funcEle";
+  return func;
+};
+
+
+var newFunc = function(){
+  var ele = func.call(this);
+  ele.name = "new";
+  var txt = document.createTextNode("New");
+  ele.appendChild(txt);
+  return ele;
+};
+
+newFunc.prototype = Object.create(func.prototype);
+
+newFunc.prototype.constructor = newFunc;
+
+var loadFunc = function(){
+  var ele = func.call(this);
+  ele.name = "load";
+  var txt = document.createTextNode("Load");
+  ele.appendChild(txt);
+  return ele;
+};
+
+loadFunc.prototype = Object.create(func.prototype);
+
+loadFunc.prototype.constructor = loadFunc;
+
+var saveFunc = function(){
+  var ele = func.call(this);
+  ele.name = "save";
+  var txt = document.createTextNode("Save");
+  ele.appendChild(txt);
+  return ele;
+};
+
+saveFunc.prototype = Object.create(func.prototype);
+
+saveFunc.prototype.constructor = saveFunc;
+
+var loadWindo = function(){
+
+};
+
+var saveWindo = function(){
+
 };
