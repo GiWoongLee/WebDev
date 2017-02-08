0a5e7870c0a7fb360a35019ceb209bdb94d87d6a
woong <anguish71@gmail.com>
2017-02-08 20:54:59 +0900

    Add login/logout function with session/cookie while modifying function of new/load(Quest 12)

--- Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
@@ -1,13 +1,26 @@
+//Make Identifier for Browser Tabs to store information on LocalStorage
+var tabID = sessionStorage.tabID && sessionStorage.closedLastTab !== '2' ? sessionStorage.tabID : sessionStorage.tabID = Math.random();
+sessionStorage.closedLastTab = '2';
+window.addEventListener('unload beforeunload', function() {
+      sessionStorage.closedLastTab = '1';
+});
+
+
 var Notepad = function() {
-	 //createButtons
+   //createButtons
    var navList = document.createElement("ul");
-   var func1 = new newFunc();
-   var func2 = new loadFunc();
-   var func3 = new saveFunc();
-   navList.appendChild(func1);
-   navList.appendChild(func2);
-   navList.appendChild(func3);
+   var newBut = new newFunc();
+   var loadBut = new loadFunc();
+   var saveBut = new saveFunc();
+   var loginBut = new loginFunc();
+   var logoutBut = new logoutFunc();
+
+   navList.appendChild(newBut);
+   navList.appendChild(loadBut);
+   navList.appendChild(saveBut);
    navList.className = "navList";
+
+   putLoginLogout(navList,loginBut,logoutBut);
 
    //createTextArea
    var txtArea = new textArea();
@@ -16,7 +29,17 @@ var Notepad = function() {
    main.appendChild(navList);
    main.appendChild(txtArea);
    document.title = "New Note";
+
 };
+
+var putLoginLogout = function(buttons,login,logout){
+  if(JSON.parse(localStorage.getItem("logged_in"))== true){
+    buttons.appendChild(logout);
+  }
+  else{
+    buttons.appendChild(login);
+  }
+}
 
 var textArea = function(){
   var txtArea = document.createElement("div");
@@ -25,12 +48,24 @@ var textArea = function(){
   txtArea.appendChild(txtNode);
   txtNode.id = "note";
 
+  txtNode.addEventListener("change",function(e){
+    if(JSON.parse(localStorage.getItem("logged_in"))==true){
+        var txt = localStorage.getItem(tabID) || "";
+        localStorage.setItem(tabID,txtNode.value);
+        var sessList = [];
+        if(localStorage.getItem("sessList")!=null){
+          sessList = JSON.parse(localStorage.getItem("sessList"));
+          if(sessList.indexOf(tabID)== -1){
+            sessList.push(tabID);
+          }
+        }
+        else{
+          sessList = [tabID];
+        }
+        localStorage.setItem("sessList",JSON.stringify(sessList));
+    }
+  })
   return txtArea;
-};
-
-textArea.removeText = function(){
-  var txt = document.getElementById("note");
-  txt.value = "";
 };
 
 textArea.readText = function(){
@@ -49,15 +84,21 @@ var func = function(){
   return func;
 };
 
-
 var newFunc = function(){
   var ele = func.call(this);
   ele.name = "new";
   var txt = document.createTextNode("New");
   ele.appendChild(txt);
   ele.addEventListener("click",function(){
-    textArea.removeText();
-    document.title = "New Note";
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET","/new",true);
+    xhr.onreadystatechange = function(){
+      if(xhr.readyState==4 && xhr.status == 200){
+        var newWindo = window.open("/");
+        newWindo.document.title = "New Note";
+      }
+    }
+    xhr.send(null);
   })
   return ele;
 };
@@ -96,6 +137,61 @@ saveFunc.prototype = Object.create(func.prototype);
 
 saveFunc.prototype.constructor = saveFunc;
 
+var loginFunc = function(){
+  var ele =func.call(this);
+  ele.name = "SignIn";
+  var txt = document.createTextNode("Sign In");
+  ele.appendChild(txt);
+  ele.addEventListener("click",function(){
+    new loginWindo();
+  });
+  return ele;
+}
+
+loginFunc.prototype = Object.create(func.prototype);
+
+loginFunc.prototype.constructor = loginFunc;
+
+var logoutFunc = function(){
+  var ele =func.call(this);
+  ele.name = "LogOut";
+  var txt = document.createTextNode("Log Out");
+  ele.appendChild(txt);
+  ele.addEventListener("click",function(){
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST","logout",true);
+    xhr.onreadystatechange = function(e){
+      if(xhr.readyState==4 && xhr.status == 200){
+        document.open();
+        document.write(xhr.responseText);
+        document.close();
+      }
+    }
+    var content = [];
+    if(localStorage.getItem("sessList")!=null){
+      var sessList = JSON.parse(localStorage.getItem("sessList"));
+      for(var i=0;i<sessList.length;i++){
+        (function(j){
+          var note = localStorage.getItem(sessList[j]);
+          content.push(note);
+        })(i);
+      }
+    }
+    localStorage.clear();
+
+    xhr.setRequestHeader("content-type","application/json");
+    xhr.send(JSON.stringify({
+      "content" : content
+    }));
+  });
+  return ele;
+}
+
+logoutFunc.prototype = Object.create(func.prototype);
+
+logoutFunc.prototype.constructor = loginFunc;
+
+
 var loadWindo = function(){
   var windo = document.createElement("div");
   windo.id = "loadwindo";
@@ -128,8 +224,11 @@ var loadWindo = function(){
               if(xhr.readyState==4 && xhr.status == 200){
                 var main = document.querySelector(".notepad");
                 main.removeChild(windo);
-                document.title = ele.textContent;
-                textArea.refreshText(xhr.responseText);
+                var newWindo = window.open("/");
+                newWindo.onload = function(){
+                  newWindo.document.title = ele.textContent;
+                  newWindo.document.getElementById("note").innerHTML = xhr.responseText;
+                }
               }
             }
             var fileName = ele.textContent;
@@ -137,7 +236,6 @@ var loadWindo = function(){
             xhr.send(JSON.stringify({"name" : fileName}));
 
           });
-
           ele.className = "fileEle";
           var txt = document.createTextNode(json[j]);
           ele.appendChild(txt);
@@ -157,8 +255,6 @@ var loadWindo = function(){
 
   var main = document.querySelector(".notepad");
   main.appendChild(windo);
-
-
 };
 
 
@@ -215,3 +311,86 @@ var saveWindo = function(){
   var main = document.querySelector(".notepad");
   main.appendChild(windo);
 };
+
+var loginWindo = function(){
+  var windo = document.createElement("div");
+  windo.id = "loginWindo";
+
+  var txt1 = document.createTextNode("ID: ")
+  var inputId = document.createElement("input");
+  inputId.id = "userID";
+  inputId.name ="userID"
+  inputId.type = "text";
+
+  var txt2 = document.createTextNode("PWD: ")
+  var inputPwd = document.createElement("input");
+  inputPwd.id= "userPwd";
+  inputPwd.name="userPwd";
+  inputPwd.type = "password";
+
+  var br =document.createElement("br");
+
+  var submitBut = document.createElement("input");
+  submitBut.className = "loginBut";
+  submitBut.type = "button";
+  submitBut.value = "Sign In";
+
+  submitBut.addEventListener('click',function(event){
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST","/login",true);
+    xhr.onreadystatechange = function(e){
+      if(xhr.readyState==4 && xhr.status==200){
+        var info = JSON.parse(xhr.responseText);
+        if(info.status=='failure') alert(info.content);
+        else{
+          //set logged_in status true and windows list in localStorage
+          localStorage.setItem("logged_in",true);
+          for(var num=0;num<info.content.length;num++){
+            (function(i){
+              var newWindo = window.open("/");
+              var txt = info.content[num];
+              newWindo.onload = function(){
+                newWindo.document.getElementById("note").value = txt;
+              }
+            })(num);
+          }
+          var main = document.querySelector(".notepad");
+          main.removeChild(windo);
+          var nav = document.querySelector(".navList");
+          var but =document.getElementsByClassName("navEle");
+          nav.removeChild(but[3]);
+          var logoutBut = new logoutFunc();
+          nav.appendChild(logoutBut);
+        }
+      }
+    }
+    xhr.setRequestHeader("content-type","application/json");
+    xhr.send(JSON.stringify({
+      userID : inputId.value,
+      userPwd : inputPwd.value
+    }));
+  })
+
+  var cancelBut = document.createElement("input");
+  cancelBut.className = "loginBut";
+  cancelBut.type = "button";
+  cancelBut.value = "Cancel";
+
+  cancelBut.addEventListener('click',function(event){
+    var main = document.querySelector(".notepad");
+    main.removeChild(windo);
+  });
+
+  windo.appendChild(txt1);
+  windo.appendChild(inputId);
+  windo.appendChild(br);
+  windo.appendChild(txt2);
+  windo.appendChild(inputPwd);
+  windo.appendChild(br);
+  windo.appendChild(submitBut);
+  windo.appendChild(cancelBut);
+
+  var main = document.querySelector(".notepad");
+  main.appendChild(windo);
+
+}
